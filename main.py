d416c3a9f3e14e4b43b67a263e27772fef77fe60
Hector Cardona <3489406370101@ingenieria.usac.edu.gt>
2026-08-02 00:10:30 -0600

    fix: corregir campo de cancion por album en la respuesta del JSON

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def get_cancion_favorita():
     return {
         "nombre": "Hector Antonio Cardona Cos",
         "carnet": "202307354",
-        "cancion_favorita": "18 Months",
+        "album": "18 Months",
         }
 
 if __name__ == "__main__":
